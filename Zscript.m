7117a8ddedfbeeeb19ae3a1bc8bb144a1f4f6ef5
gkampolis <gkampolis@gmail.com>
2014-08-03 16:40:53 +0300

    Script instead of function
    Created a script as I should have from the start. Also, some loops were
    avoided by using matrices and element-wise operations.

--- Zscript.m
+++ Zscript.m
@@ -1,13 +1,17 @@
-function [] = Zplot()
-%Zplot Calculates Z for various P and T from composition input, assuming gas
-%of C1,C2,C3 only. 
+
+% -------- Short Description --------
+%   Zscipt calculates Z for various P and T from composition input, assuming gas
+%   of C1,C2,C3 only. 
+
+% -------- Somewhat Longer Description --------
 %   User is asked to provide composition values for C1,C2 & C3 and then Z
 %   is calculated for T(50-200F) and P(14.7 and 100-2000 psia).Main output 
 %   is a plot of Z vs P for four different T.
 %   Secondary output is a table of Z values for the various P & T. Currently
 %   the table is not displayed by default, however this can be changed by
 %   removing the percent sign "%" from the penultimate line of the function's
-%   .m file.
+%   .m file. Furthermore, note that Z is calculated by an algorithm
+%   avoiding loops.
 
 %% --- 1. Input ---
 
@@ -45,22 +49,17 @@ Tf = [50:50:200];           % Creates Temperature vector (in Fahrenheit) with th
 Tr = Tf+459.67;             % Converts the above to Rankine. Note that Tf(i) corresponds to Tr(i), e.g. Tf(3)= 150 & Tr(3)=150+459.67=Tf(3)+459.67
 Ppr = Ppsia / Ppc;          % Calc of pseudo-reduced Pressure vector
 Tpr = Tr/ Tpc;              % Calc of pseudo-reduced temperature vector
+Ppr_mat=repmat(Ppr',1,length(Tpr)); % Create new Pressure matrix by repeating previous P values 4 times (for 4 different Temperature values) for element-wise calculations
+Tpr_mat=repmat(Tpr,length(Ppr),1);  % Create new Temperature matrix by repeating previous T values 21 times (for 21 different Pressure values) for element-wise calculations
 
-% -- 2.2 Calc of Z
+% -- 2.2 Calc of Z without loops
 
-Z = zeros(4,21);  % Preallocate matrix, where the four rows correspond to four different temperatures, 50/100/150/200 F
-for i=1:1:4
-    % Effectively i sets working temperature and j working pressure
-    % A,C,D are functions of Temperature alone and therefore are calculated in the i-loop
-    A=1.39*(Tpr(i)-0.92)^0.5-0.36*Tpr(i)-0.101;
-    C=0.132-0.32*log10(Tpr(i));
-    D=10^(0.3106-0.49*Tpr(i)+0.1824*Tpr(i)^2);
-    for j=1:1:21
-        B=(0.62-0.23*Tpr(i))*Ppr(j)+(0.066/(Tpr(i)-0.86)-0.037)*Ppr(j)^2+0.32/(10^(9*(Tpr(i)-1)))*Ppr(j)^6;
-        Z(i,j)=A+(1-A)/exp(B)+C*Ppr(j)^D;
-    end
-end
-
+A= 1.39*(Tpr_mat-0.92).^0.5-0.36*Tpr_mat-0.101;
+C= 0.132-0.32*log10(Tpr_mat);
+D= 10.^(0.3106-0.49*Tpr_mat+0.1824*Tpr_mat.^2);
+B= (0.62-0.23*Tpr_mat).*Ppr_mat+(0.066./(Tpr_mat-0.86)-0.037).*Ppr_mat.^2+0.32./(10.^(9.*(Tpr_mat-1))).*Ppr_mat.^6;
+Znl= A+(1-A)./exp(B)+C.*Ppr_mat.^D;  % nl simply stands for no-loop.
+Z= Znl';
 %% --- 3. Output ---
 
 % -- 3.1 Plot Z vs P
@@ -79,6 +78,6 @@ legend('T=50F','T=100F','T=150F','T=200F')
 %Temperature (50, 100, 150, 200) and each column to a given Pressure
 %(14.7,100,200...2000|N=21 values, 20 for 100...2000 plus 14.7).
 
-% TABLE=array2table(Z)
-end
+
+TABLE=array2table(Z)
 
